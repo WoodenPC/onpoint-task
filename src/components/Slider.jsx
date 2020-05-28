0e36e582c33d6e2560d61a9e227c8797e14a355e
Arslanov Rasul <stiflerovich@mail.ru>
2020-05-28 19:03:47 +0500

    доделал слайдер, докинул инфы в ридми

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { cn } from '@bem-react/classname';
 
 const classes = cn('Slider');
 
 const Slider = React.memo(({ values = [], min = 0, max = 1, step = 1, value = 0, onChange, classMix = '' }) => {
+  const [innerVal, setInnerVal] = useState(Math.round(value / (values.length - 1) * 100));
+  const handleChange = useCallback((e) => {
+    const { value } = e.target;
+    setInnerVal(value);
+  }, []);
+
+  const handleTouchEnd = useCallback(() => {
+    // костыльненько
+    const normalizedValue = innerVal / 100;
+    const normalizedIndex = Math.round(normalizedValue * (values.length - 1));
+    onChange({ target: { value: normalizedIndex } });
+    setInnerVal(Math.round(normalizedIndex / (values.length - 1) * 100));
+  }, [values, onChange, innerVal]);
+
   return (
     <div className={`${classes()} ${classMix}`}>
       <input
-        min={min}
-        step={step}
-        max={max}
-        value={value}
-        onChange={onChange}
-        onInput={onChange}
+        min={0}
+        step={1}
+        max={100}
+        value={innerVal}
+        onChange={handleChange}
+        onTouchEnd={handleTouchEnd}
+        onInput={handleTouchEnd}
         type='range'
         className={classes('Input')}
         style={{
-          background: `linear-gradient(to right, #d1eaff ${value / max * 100}%, 
-            #435063 ${value / max * 100}%)`
+          background: `linear-gradient(to right, #d1eaff ${innerVal}%, 
+            #435063 ${innerVal}%)`
         }}
       />
       <div className={classes('Ticks')}>
